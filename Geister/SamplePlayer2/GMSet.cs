f37ad8e60d336a9e4b13cd5b335e208026373630
KensCup2017 <kenscup2017@gmail.com>
2017-05-19 11:10:37 +0900

    SamplePlayer2 追加

--- Geister/SamplePlayer2/GMSet.cs
+++ Geister/SamplePlayer2/GMSet.cs
@@ -6,13 +6,15 @@ using System.Threading.Tasks;
 using Geister.GameSystem;
 using Geister.GameInformation;
 
-namespace Geister.SamplePlayer2 {
+namespace Geister.SamplePlayer2
+{
     /// <summary>
     /// Ghostの場所と移動方向を引数に与えると
     /// 移動可能かどうかを判定する
     /// 移動可能な場合には，次の場所，移動すれば敵の駒がとれるか，移動したら敵の駒に取られてしまわないかを判定する．
     /// </summary>
-    public class GhostMoveSet {
+    public class GhostMoveSet
+    {
 
         public Ghost ghost = null;
         public GhostMove direction;
@@ -26,7 +28,8 @@ namespace Geister.SamplePlayer2 {
         public int point = 0;
 
 
-        public GhostMoveSet(Ghost g, GhostMove m) {
+        public GhostMoveSet(Ghost g, GhostMove m)
+        {
             ghost = g;
             direction = m;
         }
@@ -34,37 +37,60 @@ namespace Geister.SamplePlayer2 {
         /// <summary>
         /// 駒の移動に対する評価を計算する(式は適当)
         /// </summary>
-        public void culPoint() {
+        public void culPoint()
+        {
             point = 0;
-            if (ghost.Gt == GhostAttribute.evil) {
-                if (direction == GhostMove.Down) point = point + 1;
+            if (current_catched) point = point + 20;
 
+            if (ghost.Gt == GhostAttribute.evil)
+            {
+                Random cRandom = new System.Random();
+                point = point + cRandom.Next(15);
             }
-            if (ghost.Gt == GhostAttribute.good) {
-                if (direction == GhostMove.Up) point = point + 2;
-                if (direction == GhostMove.Down) point = point - 2;
+
+            if (ghost.Gt == GhostAttribute.good)
+            {
+                if (direction == GhostMove.Up) point = point + 3;
+                if (direction == GhostMove.Down)
+                    if (ghost.P.Col != 0) point = point - 2;
+                    else point = point + 1;
                 if (direction == GhostMove.Right && ghost.P.Col >= 3) point = point + 1;
                 if (direction == GhostMove.Left && ghost.P.Col < 3) point = point + 1;
             }
 
-
-            if (ghost.Gt == GhostAttribute.good) {
-                if (next.Row == -1 && (next.Col == 0 || next.Col == 5)) {
+            if (ghost.Gt == GhostAttribute.good)
+            {
+                if (next.Row == -1 && (next.Col == 0 || next.Col == 5))
+                {
                     point = point + 1000000;
                 }
             }
- 
-            if (catchable) point = point + 25;
-            if (catchable && !next_catched) point =point+ 100;
+            if (ghost.Gt == GhostAttribute.evil)
+            {
 
-            
-            if (next_catched) {
-                if (ghost.Gt == GhostAttribute.evil) point = point + 20;
-                if (ghost.Gt == GhostAttribute.good) point = point - 50;
+                if (current_catched && next_catched && catchable) point = point + 250;
+                else if (current_catched && next_catched && !catchable) point = point + 0;
+                else if (current_catched && !next_catched && catchable) point = point + 150;
+                else if (current_catched && !next_catched && !catchable) point = point + 0;
+                else if (!current_catched && next_catched && catchable) point = point + 200;
+                else if (!current_catched && next_catched && !catchable) point = point + 50;
+                else if (!current_catched && !next_catched && catchable) point = point + 100;
+                else if (!current_catched && !next_catched && !catchable) point = point + 0;
+
             }
-            if (current_catched && !next_catched) {
-                if (ghost.Gt == GhostAttribute.good) point = point + 100;
-                if (ghost.Gt == GhostAttribute.evil) point = point + 80;
+            if (ghost.Gt == GhostAttribute.good)
+            {
+                point = point + 2;
+                if (current_catched && next_catched && catchable) point = point + 50;
+                else if (current_catched && next_catched && !catchable) point = point - 50;
+
+                else if (current_catched && !next_catched && catchable) point = point + 250;
+                else if (current_catched && !next_catched && !catchable) point = point + 200;
+
+                else if (!current_catched && next_catched && catchable) point = point - 50;
+                else if (!current_catched && next_catched && !catchable) point = point - 100;
+                else if (!current_catched && !next_catched && catchable) point = point + 100;
+                else if (!current_catched && !next_catched && !catchable) point = point + 0;
 
             }
 
--- Geister/SamplePlayer2/TestPlayer2.cs
+++ Geister/SamplePlayer2/TestPlayer2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
@@ -9,12 +10,12 @@ using Geister.SamplePlayer2;
 
 
 //競合を防ぐため，名前空間をユニークなものに変更してプログラムを作成してください．
-namespace Geister.SamplePlayer2 {
+namespace Geister.SamplePlayer2
+{
     /// <summary>
-    /// 1日クオリティ
     /// 
     /// アルゴリズム
-    /// 現状態と移動後状態で適当にスコアリングして移動法を決めるサンプルプレイヤー
+    /// 現状態と移動後状態でスコアリングして移動法を決めるサンプルプレイヤー
     /// 初期配置ランダム
     /// goodを取られないように移動
     /// 敵に取られず敵の駒を取れる場合にはすぐ取る
@@ -25,7 +26,8 @@ namespace Geister.SamplePlayer2 {
     /// 
     /// </summary>
     /// 
-    class TestPlayer2 : AbstractPlayer {
+    class TestPlayer2 : AbstractPlayer
+    {
 
         FieldObject myID;
         FieldObject enemyID;
@@ -33,11 +35,13 @@ namespace Geister.SamplePlayer2 {
         FieldObject[,] boardState;
         /// <summary>
         /// 以下の2つのコンストラクタ内でゴーストの初期配置を設定してください．
-        public TestPlayer2() : base() {
+        public TestPlayer2() : base()
+        {
             SetInitialPlacement(RandPlace());
         }
 
-        public TestPlayer2(string name) : base() {
+        public TestPlayer2(string name) : base()
+        {
             this.name = name;
             SetInitialPlacement(RandPlace());
 
@@ -46,13 +50,15 @@ namespace Geister.SamplePlayer2 {
         /// 初期位置をランダム配置する
         /// </summary>
         /// <returns></returns>
-        private GhostAttribute[,] RandPlace() {
+        private GhostAttribute[,] RandPlace()
+        {
             GhostAttribute[] randplace = new GhostAttribute[8] { GhostAttribute.evil, GhostAttribute.evil, GhostAttribute.evil, GhostAttribute.evil ,
                                                       GhostAttribute.good, GhostAttribute.good, GhostAttribute.good, GhostAttribute.good };
 
             System.Random rng = new System.Random();
             int n = randplace.Length;
-            while (n > 1) {
+            while (n > 1)
+            {
                 n--;
                 int k = rng.Next(n + 1);
                 GhostAttribute tmp = randplace[k];
@@ -70,7 +76,8 @@ namespace Geister.SamplePlayer2 {
         /// ゴーストがランダムに動くサンプルプログラム
         /// </summary>
         /// <returns></returns>
-        public override Move GetMove() {
+        public override Move GetMove()
+        {
 
             boardState = GetBoardState();
             myID = GetMyPlayerID();
@@ -79,42 +86,45 @@ namespace Geister.SamplePlayer2 {
             {
                 enemyID = FieldObject.P2;
             }
-            else if(myID.Equals(enemyID))
+            else if (myID.Equals(FieldObject.P2))
             {
                 enemyID = FieldObject.P1;
             }
 
 
             List<Ghost> glist = GetMyGhostList().OrderBy(j => Guid.NewGuid()).ToList();
+
             List<GhostMove> gmlist = new List<GhostMove>();
             gmlist.Add(GhostMove.Down);
             gmlist.Add(GhostMove.Left);
             gmlist.Add(GhostMove.Right);
             gmlist.Add(GhostMove.Up);
-
-
+            gmlist.OrderBy(j => Guid.NewGuid()).ToList();
             Move m = null;
             GhostMoveSet gms = null;
 
             int point = -500000;
 
-            for (int i = 0; i < GetMyGhostList().Count; i++) {
+            for (int i = 0; i < GetMyGhostList().Count; i++)
+            {
                 Ghost g = glist[i];
 
-                foreach (GhostMove gm in gmlist) {
+                foreach (GhostMove gm in gmlist)
+                {
                     gms = new GhostMoveSet(g, gm);
-                    if (IsMovable(g.P, gm)) {
+                    if (IsMovable(g.P, gm))
+                    {
 
                         gms.next = NextPosition(g.P, gm);
                         gms.catchable = isCatchable(gms.next);
+
                         gms.next_catched = isCatched(gms.next);
                         gms.current_catched = isCatched(gms.ghost.P);
                         gms.culPoint();
 
-                        System.Console.WriteLine("serected  {0},{1},{2}", gms.ghost.P.Row, gms.ghost.P.Col, gms.direction);
-                        System.Console.WriteLine("serected  {0},{1},{2}", gms.catchable, gms.next_catched, gms.point);
 
-                        if (gms.point > point) {
+                        if (gms.point > point)
+                        {
                             point = gms.point;
                             m = new Move(g.P, gm);
                         }
@@ -122,8 +132,6 @@ namespace Geister.SamplePlayer2 {
                 }
 
             }
-
-
 
             return m;
         }
@@ -134,9 +142,11 @@ namespace Geister.SamplePlayer2 {
         /// 移動した場合に相手の駒をとれるか判定
         /// </summary>
         /// <returns></returns>
-        private Boolean isCatchable(Position p) {
+        private Boolean isCatchable(Position p)
+        {
             List<Position> enemyGhostList = GetGhostPositionList(enemyID);
-            foreach (Position enemyGhost in enemyGhostList) {
+            foreach (Position enemyGhost in enemyGhostList)
+            {
                 if (p == enemyGhost) return true;
             }
             return false;
@@ -146,20 +156,28 @@ namespace Geister.SamplePlayer2 {
         /// 移動した場合次のターンで取られてしまうか判定
         /// </summary>
         /// <returns></returns>
-        private Boolean isCatched(Position next) {
+        private Boolean isCatched(Position posi)
+        {
             List<Position> enemyGhostList = GetGhostPositionList(enemyID);
-            foreach (Position enemyGhost in enemyGhostList) {
-                if (next.Row == enemyGhost.Row) {
-                    if (next.Col == enemyGhost.Col + 1 || next.Col == enemyGhost.Col - 1) {
+
+            foreach (Position enemyGhost in enemyGhostList)
+            {
+                if (posi.Row == enemyGhost.Row)
+                {
+                    if (posi.Col == enemyGhost.Col + 1 || posi.Col == enemyGhost.Col - 1)
+                    {
                         return true;
                     }
                 }
-                if (next.Col == enemyGhost.Col) {
-                    if (next.Row == enemyGhost.Row + 1 || next.Row == enemyGhost.Row - 1) {
+                if (posi.Col == enemyGhost.Col)
+                {
+                    if (posi.Row == enemyGhost.Row + 1 || posi.Row == enemyGhost.Row - 1)
+                    {
                         return true;
                     }
                 }
             }
+
             return false;
         }
 
@@ -169,7 +187,8 @@ namespace Geister.SamplePlayer2 {
         /// <param name="current"></param>
         /// <param name="gm"></param>
         /// <returns></returns>
-        public Position NextPosition(Position current, GhostMove direction) {
+        public Position NextPosition(Position current, GhostMove direction)
+        {
             Position next_p = current.Clone();
             if (direction == GhostMove.Down) next_p.Row++;
             if (direction == GhostMove.Up) next_p.Row--;
@@ -185,43 +204,62 @@ namespace Geister.SamplePlayer2 {
         /// <param name="p"></param>
         /// <param name="gm"></param>
         /// <returns></returns>
-        private Boolean IsMovable(Position p, GhostMove gm) {
-            if (Exists(p)) {
-                if (gm == GhostMove.Down) {
-                    if (p.Row == 5) {
+        private Boolean IsMovable(Position p, GhostMove gm)
+        {
+            if (Exists(p))
+            {
+                if (gm == GhostMove.Down)
+                {
+                    if (p.Row == 5)
+                    {
                         return false;
                     }
 
-                    if (boardState[p.Row + 1, p.Col] == GetMyPlayerID()) {
+                    if (boardState[p.Row + 1, p.Col] == GetMyPlayerID())
+                    {
                         return false;
                     }
-                } else if (gm == GhostMove.Left) {
-                    if (p.Col == 0) {
-                        return false;
-                    }
-
-                    if (boardState[p.Row, p.Col - 1] == GetMyPlayerID()) {
-                        return false;
-                    }
-                } else if (gm == GhostMove.Right) {
-                    if (p.Col == 5) {
+                }
+                else if (gm == GhostMove.Left)
+                {
+                    if (p.Col == 0)
+                    {
                         return false;
                     }
 
-                    if (boardState[p.Row, p.Col + 1] == GetMyPlayerID()) {
+                    if (boardState[p.Row, p.Col - 1] == GetMyPlayerID())
+                    {
                         return false;
                     }
-                } else if (gm == GhostMove.Up) {
-                    if (p.Row == 0) {
-                        if (p.Col == 0 || p.Col == 5) {
-                            if (GetMyGhostAttribute(p) == GhostAttribute.good) {
+                }
+                else if (gm == GhostMove.Right)
+                {
+                    if (p.Col == 5)
+                    {
+                        return false;
+                    }
+
+                    if (boardState[p.Row, p.Col + 1] == GetMyPlayerID())
+                    {
+                        return false;
+                    }
+                }
+                else if (gm == GhostMove.Up)
+                {
+                    if (p.Row == 0)
+                    {
+                        if (p.Col == 0 || p.Col == 5)
+                        {
+                            if (GetMyGhostAttribute(p) == GhostAttribute.good)
+                            {
                                 return true;
                             }
                         }
                         return false;
                     }
 
-                    if (boardState[p.Row - 1, p.Col] == GetMyPlayerID()) {
+                    if (boardState[p.Row - 1, p.Col] == GetMyPlayerID())
+                    {
                         return false;
                     }
                 }
@@ -240,9 +278,12 @@ namespace Geister.SamplePlayer2 {
         /// </summary>
         /// <param name="p"></param>
         /// <returns></returns>
-        private Boolean Exists(Position p) {
-            foreach (Ghost g in GetMyGhostList()) {
-                if (g.P == p) {
+        private Boolean Exists(Position p)
+        {
+            foreach (Ghost g in GetMyGhostList())
+            {
+                if (g.P == p)
+                {
                     return true;
                 }
             }
